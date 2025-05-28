96d5280980ff97509a2a1e5df1d9abf733c26751
Balamut <Balamut878S@ya.ru>
2025-05-28 13:46:04 +0300

    Initial commit

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Александр Дудченко on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
